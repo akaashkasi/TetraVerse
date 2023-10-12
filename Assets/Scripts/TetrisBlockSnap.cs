f6cc05d8ea37403e2af03cf516a8381c4e626c64
madhavimani9 <madhavimani9@gmail.com>
2023-10-12 14:53:45 -0500

    made game "levels"

--- Assets/Scripts/TetrisBlockSnap.cs
+++ Assets/Scripts/TetrisBlockSnap.cs
@@ -6,7 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TetrisBlockSnap : MonoBehaviourPun //attached to each tetris block
 {
-    //todo: evan add public AudioSource audio
+    //todo: Evan add public AudioSource audio
     public float gridSize = 0.5f; // Size of each grid square
     public float offset = 0.5f / 2.0f;
 
@@ -14,6 +14,19 @@ public class TetrisBlockSnap : MonoBehaviourPun //attached to each tetris block
     public float maxX = 1.5f;  // Maximum X bound
     public float minZ = -2.25f; // Minimum Z bound
     public float maxZ = 2.25f;  // Maximum Z bound
+
+    /**private void Update()
+    {
+        if (this.GetComponent<XRGrabNetworkInteractable>().isSelected)
+        {
+            PhotonView PV = this.GetComponent<PhotonView>();
+
+            if (PV.IsMine) // If the photon view component that I am interacting with, is owned by me
+            {
+                //TODO: glow by Akaash
+            }
+        }
+    }*/
 
     public void OnCollisionEnter(Collision collision)
     {
@@ -27,7 +40,11 @@ public class TetrisBlockSnap : MonoBehaviourPun //attached to each tetris block
                 //TODO: jumping more than needed
                 //TODO: shaking/buggy
                 //TODO: do I need to customize the script for each of the blocks?
-                Vector3 snapPosition = new Vector3(Mathf.Round(collisionPoint.x / gridSize) * gridSize + offset, 0.25f,Mathf.Round(collisionPoint.z / gridSize) * gridSize + offset);
+                //Vector3 snapPosition = new Vector3(Mathf.Round(collisionPoint.x / gridSize) * gridSize + offset, 0.25f,Mathf.Round(collisionPoint.z / gridSize) * gridSize + offset);
+                Vector3 snapPosition = new Vector3(
+                     Mathf.Round(collisionPoint.x / gridSize) * gridSize + offset,
+                     Mathf.Round(collisionPoint.y / gridSize) * gridSize + offset,
+                     Mathf.Round(collisionPoint.z / gridSize) * gridSize + offset);
                 Debug.Log("Snap pos" + snapPosition);
 
                 //TODO: don't want it to snap to being outside
@@ -36,23 +53,23 @@ public class TetrisBlockSnap : MonoBehaviourPun //attached to each tetris block
 
                 this.transform.position = snapPosition;
 
-                //TODO: evan play sound
 
                 //want it to stay in same rotation
                 //TODO: rotation not working
-                float angle = Mathf.Round(this.transform.rotation.eulerAngles.y / 90.0f) * 90.0f;
-                Quaternion newRotation = Quaternion.Euler(0, angle, 0);
+                float anglex = Mathf.Round(this.transform.rotation.eulerAngles.x / 90.0f) * 90.0f;
+                float angley = Mathf.Round(this.transform.rotation.eulerAngles.y / 90.0f) * 90.0f;
+                float anglez = Mathf.Round(this.transform.rotation.eulerAngles.z / 90.0f) * 90.0f;
+                Quaternion newRotation = Quaternion.Euler(anglex, angley, anglez);
+
                 this.transform.rotation = newRotation;
+
+                //TODO: Evan play sound
 
                 //Freeze it so it doesn't move anymore
                 this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
                 
 
-                //2. set the corresponding floor positions to occupied
-                //int gridX = Mathf.FloorToInt(snapPosition.x / gridSize);
-                //int gridY = Mathf.FloorToInt(snapPosition.z / gridSize);
-                //TODO: but it could be more than just one position!
-                //grid[gridX, gridY] = true;
+                //2. set the corresponding floor positions to occupied and compute points
 
                 //3. disable grab:
                 this.gameObject.GetComponent<XRGrabNetworkInteractable>().enabled = false;
