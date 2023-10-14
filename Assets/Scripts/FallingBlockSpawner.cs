cdac45b612b41bad4a07987dc353d991216502dd
madhavimani9 <madhavimani9@gmail.com>
2023-10-14 11:45:05 -0500

    countdown numbers, boundaries, spawn with rotation, snap for all blocks
    - snap still needs some work, need to figure out audio

--- Assets/Scripts/FallingBlockSpawner.cs
+++ Assets/Scripts/FallingBlockSpawner.cs
@@ -7,13 +7,14 @@ using Photon.Pun;
 
 public class FallingBlockSpawner : MonoBehaviourPun
 {
-    private float countdownInterval = 1.0f;
+    private const float countdownInterval = 1.0f;
     private float countdownTime = 3.0f;
 
     private string[] tetrisBlockPrefabsNames;
     private float[] xzRangePositions;
+    private int[] blockRotations;
 
-    private float spawnHeight = 6.5f;
+    private const float spawnHeight = 6.5f;
 
     private float spawnTimer;
 
@@ -30,38 +31,43 @@ public class FallingBlockSpawner : MonoBehaviourPun
     private int level3NumBlocksThreshold = 6; //35
 
     private float currSpawnInterval;
-    private float level0SpawnInterval = 10f; 
-    private float level1SpawnInterval = 8f; 
-    private float level2SpawnInterval = 6f;
-    private float level3SpawnInterval = 4f; 
+    private const float level0SpawnInterval = 10f; 
+    private const float level1SpawnInterval = 8f; 
+    private const float level2SpawnInterval = 6f;
+    private const float level3SpawnInterval = 4f; 
 
-    private float level0LinearDrag = 20.0f; //doing dramatic just to see if it works as expected
-    private float level1LinearDrag = 10.0f;
-    private float level2LinearDrag = 5.0f;
-    private float level3LinearDrag = 3.0f;
+    private const float level0LinearDrag = 20.0f; //TODO: fine tune values
+    private const float level1LinearDrag = 10.0f;
+    private const float level2LinearDrag = 5.0f;
+    private const float level3LinearDrag = 3.0f;
+
+    private const int rotation90 = 90;
+    private const int rotation180 = 180;
 
     private void Start()
     {
         curLevel = Level0;
-
         numBlocksSpawned = 0;
         currSpawnInterval = level0SpawnInterval; //initial interval
 
         //string[] blockNames = { "I-Block", "J-Block", "L-Block", "S-Block", "Square-Block", "T-Block", "Z-Block" };
-        string[] blockNames = { "I-Block", "J-Block"};
+        string[] blockNames = { "I-Block"};
+        tetrisBlockPrefabsNames = blockNames;
 
-        float[] xzRange = { -0.75f, -0.25f, 0.25f, 0.75f}; //TODO: figure out how to do the range better. potentially dictionary for each block- array of possible values
+        int[] rotations = { 0, rotation90, rotation180};
+        blockRotations = rotations;
 
+        //float[] xzRange = { -0.75f, -0.25f, 0.25f, 0.75f}; //TODO: figure out how to do the range better. potentially dictionary for each block- array of possible values
+        float[] xzRange = { -1.25f };
         xzRangePositions = xzRange;
 
-        tetrisBlockPrefabsNames = blockNames;
     }
     private void Update() 
     {
         if (PhotonNetwork.IsMasterClient)
         {
             spawnTimer += Time.deltaTime;
-            if (countdownTime > 0) //TODO: okay way to check 1 sec?
+            if (countdownTime > 0) 
             {
                 if (spawnTimer >= countdownInterval)
                 {
@@ -79,8 +85,7 @@ public class FallingBlockSpawner : MonoBehaviourPun
 
                     numBlocksSpawned++;
 
-                    //After a set number of blocks which have been spawned, decrease the time between the blocks spawning.
-                    //Move onto next "level"
+                    //After a set number of blocks which have been spawned, move onto next "level"
                     if (numBlocksSpawned > level3NumBlocksThreshold)
                     {
                         curLevel = Level3;
@@ -106,13 +111,16 @@ public class FallingBlockSpawner : MonoBehaviourPun
     private void SpawnCountdownBlock()
     {
         string time = "Num" + ((int) countdownTime).ToString();
-        Vector3 spawnPosition = new Vector3(0, spawnHeight, 0);
+        Vector3 spawnPosition = new Vector3(-0.5f, spawnHeight, 0.75f);
         GameObject newBlock = PhotonNetwork.Instantiate(time, spawnPosition, Quaternion.identity);
         Rigidbody rb = newBlock.GetComponent<Rigidbody>();
+
         if (rb != null)
         {
             rb.useGravity = true;
-            rb.drag = 2.0f;
+            newBlock.transform.rotation = Quaternion.Euler(90, 180, 0);
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
+            rb.drag = 3.0f;
         }
     }
 
@@ -121,8 +129,7 @@ public class FallingBlockSpawner : MonoBehaviourPun
         int randomIndex = Random.Range(0, tetrisBlockPrefabsNames.Length);
         string chosenBlockPrefabName = tetrisBlockPrefabsNames[randomIndex];
 
-        // Calculate the spawn position above the ceiling
-
+        //1. Determine drop position
         int randomXIndex = Random.Range(0, xzRangePositions.Length);
         float xPos = xzRangePositions[randomXIndex];
         int randomZIndex = Random.Range(0, xzRangePositions.Length);
@@ -130,15 +137,19 @@ public class FallingBlockSpawner : MonoBehaviourPun
 
         Vector3 spawnPosition = new Vector3(xPos, spawnHeight, zPos);
 
+        //2. Determine drop rotation
+        int randomRotationIndexZ = Random.Range(0, blockRotations.Length);
+        int zRotation = blockRotations[randomRotationIndexZ];
+
         GameObject newBlock = PhotonNetwork.Instantiate(chosenBlockPrefabName, spawnPosition, Quaternion.identity);
 
         Rigidbody rb = newBlock.GetComponent<Rigidbody>();
         if (rb != null)
         {
+            newBlock.transform.rotation = Quaternion.Euler(0, 0, zRotation);
             rb.useGravity = true;
 
-            //change the pace at which the object falls based on the level
-
+            //3. Change the pace at which the object falls based on the level
             if (curLevel == Level0)
             {
                 rb.drag = level0LinearDrag;
